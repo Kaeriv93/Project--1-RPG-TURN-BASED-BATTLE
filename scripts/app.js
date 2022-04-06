66692735b242fb73be76d89c65996edaf67c578e
kaeriv93 <vsan93@yahoo.com>
2022-04-06 12:36:46 -0500

    Creating a class for heroes

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,18 @@
+class Heroes{
+    constructor(name,health,mana,attack,block,magic,items){
+        this.name = name;
+        this.health = health
+        this.mana = mana;
+        this.attack = attack;
+        this.block = block;
+        this.magic = magic;
+        this.items= items;
+    }
+}
+
+const cloud = new Heroes('Cloud', 200, 100, 50,25,'magic','items');
+console.log(cloud);
+
 let $musictest = $('button.musictest');
 
 console.log($musictest)
@@ -6,3 +21,5 @@ $musictest.click(()=>{
     const battleMusic = new Audio("./music/battle.mp3");
     battleMusic.play();
 })
+
+
