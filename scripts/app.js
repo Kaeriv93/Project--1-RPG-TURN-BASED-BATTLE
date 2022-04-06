224311f7e4e7f80822f9e82e4c0cafe48635e1ef
kaeriv93 <vsan93@yahoo.com>
2022-04-06 14:29:01 -0500

    Did more testing with attack and health such, will refactor later. Adding in character models now

--- scripts/app.js
+++ scripts/app.js
@@ -1,53 +1,94 @@
 
 // Hero and Villain Class
 class Heroes{
-    constructor(name,health,mana,strength,block,magicdmg,items){
+    constructor(name,health,maxHealth,mana,maxMana,minStr,maxStr,minMag,maxMag,block){
         this.name = name;
-        this.health = health
+        this.health = health;
+        this.maxhealth = maxHealth
         this.mana = mana;
-        this.strength= strength;
+        this.maxMana = maxMana
+        this.minStr = minStr;
+        this.maxStr = maxStr;
+        this.minMag = minMag;
+        this.maxMag = maxMag;
         this.block = block;
-        this.magicdmg = magicdmg;
-        this.items= items;
     }
      attack(target) {
-        
-        target.health -= this.strength
+        let damage = Math.floor(Math.random()*(this.maxStr-this.minStr + 1)+this.minStr);
+        target.health -= damage
     }
      redPotion(target){
-        if(target.health > 0){
+        if(target.health > 0 ){
             target.health += 100
             
         } 
      }
      bluePotion(target){
+         if (this.bluePotion > target.maxMana){
+             return false
+         }
          target.mana += 50
      }
      magic(target){
          target.health -= this.magicdmg
+         this.mana -= 70
      } 
 }
 
 
+const sephiroth = new Heroes('Sephiroth',2000,2000,400,400,10,50,30,70,20);
+const cloud = new Heroes('Cloud', 200, 200, 100, 100, 20,50,30,80,40);
+const tifa = new Heroes('Tifa', 150, 150,200,200,12,35,60,120,30);
+// END OF CLASS OBJECT
 
 
 
+// Variables
+let $cloud = $('.cloud')
+let $cloudattack = $('.cloud-attack')
+let currentTurn = 0
+console.log($cloud)
 
-const cloud = new Heroes('Cloud', 200, 100, 50, 25,60,'items');
-const tifa = new Heroes('Tifa', 150, 200, 35, 20, 75, 'items')
+
+
+const cloudAttack =() =>{
+    cloud.attack(sephiroth);
+    $cloud.attr('src', './images/Cloud/cloud-attack.gif')
+}
+
+$cloudattack.click(()=>{
+    cloudAttack();
+    // $cloud.attr('src', './images/Cloud/cloud-idle.gif')
+})
+
 console.log(cloud);
 console.log(tifa)
-
-cloud.attack(tifa)
-cloud.attack(tifa)
-cloud.attack(tifa)
-tifa.magic(cloud)
-console.log(tifa)
-console.log(cloud)
-cloud.bluePotion(tifa)
-console.log(tifa)
+console.log(sephiroth)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// MUSIC AND SOUND EFFECTS
 let $musictest = $('button.musictest');
 
 console.log($musictest)
@@ -55,6 +96,7 @@ console.log($musictest)
 $musictest.click(()=>{
     const battleMusic = new Audio("./music/battle.mp3");
     battleMusic.play();
+    
 })
 
 
