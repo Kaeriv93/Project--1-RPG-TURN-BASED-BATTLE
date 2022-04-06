d59bbfe0b6db0ea63f5e6c2bab492b5394205b61
kaeriv93 <vsan93@yahoo.com>
2022-04-06 13:12:42 -0500

    Created character classes and basic functions ran console logs to run functionality. All tests passed. Working on modal to give user instructions

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,52 @@
+
+// Hero and Villain Class
 class Heroes{
-    constructor(name,health,mana,attack,block,magic,items){
+    constructor(name,health,mana,strength,block,magicdmg,items){
         this.name = name;
         this.health = health
         this.mana = mana;
-        this.attack = attack;
+        this.strength= strength;
         this.block = block;
-        this.magic = magic;
+        this.magicdmg = magicdmg;
         this.items= items;
     }
+     attack(target) {
+        
+        target.health -= this.strength
+    }
+     redPotion(target){
+        if(target.health > 0){
+            target.health += 100
+            
+        } 
+     }
+     bluePotion(target){
+         target.mana += 50
+     }
+     magic(target){
+         target.health -= this.magicdmg
+     } 
 }
 
-const cloud = new Heroes('Cloud', 200, 100, 50,25,'magic','items');
+
+
+
+
+
+const cloud = new Heroes('Cloud', 200, 100, 50, 25,60,'items');
+const tifa = new Heroes('Tifa', 150, 200, 35, 20, 75, 'items')
 console.log(cloud);
+console.log(tifa)
+
+cloud.attack(tifa)
+cloud.attack(tifa)
+cloud.attack(tifa)
+tifa.magic(cloud)
+console.log(tifa)
+console.log(cloud)
+cloud.bluePotion(tifa)
+console.log(tifa)
+
 
 let $musictest = $('button.musictest');
 
