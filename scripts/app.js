dc83e2a123e7a82cdda92cd702efdb4b590048d5
kaeriv93 <vsan93@yahoo.com>
2022-04-06 22:01:11 -0500

    Worked on getting the battle functionality to be more smooth, players can no longer heal themselves more than their max health and mana points. Magic attacks can not be casted if there is not enough mana points. Potions will heal if there is none in your inventory

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,18 @@
 
 // Hero and Villain Class
 class Heroes{
-    constructor(name,health,maxHealth,mana,maxMana,minStr,maxStr,minMag,maxMag,block){
+    constructor(name,health,maxHealth,mana,maxMana,minStr,maxStr,minMag,maxMag,potions,manapotions){
         this.name = name;
         this.health = health;
-        this.maxhealth = maxHealth
+        this.maxHealth = maxHealth
         this.mana = mana;
         this.maxMana = maxMana
         this.minStr = minStr;
         this.maxStr = maxStr;
         this.minMag = minMag;
         this.maxMag = maxMag;
-        this.block = block;
+        this.potions = potions;
+        this.manapotions = manapotions
     }
      attack(target) {
         let damage = Math.floor(Math.random()*(this.maxStr-this.minStr + 1)+this.minStr);
@@ -20,36 +21,53 @@ class Heroes{
         console.log(`${target.name} receieved amount of ${damage} damage!`)
     }
      redPotion(target){
-        if(target.health > 0 ){
-            target.health += 100
-            console.log(`${target.name} recovered 100 hp! `)
-            
-        } 
-    }
-    bluePotion(target){
-        if (this.bluePotion > target.maxMana){
-            return false
-        }
-        target.mana += 50
-    }
-    magic(target){
-        let magicalDamage = Math.floor(Math.random()*(this.maxMag-this.minMag + 1)+this.minMag);
-        target.health -= magicalDamage
-        this.mana -= 70
+              
+             if(target.health > 0 && this.potions > 0 && target.health < target.maxHealth ){
+                 target.health += 100
+                 if(target.health >= target.maxHealth){
+                     target.health =  target.maxHealth
+                 }
+                 console.log(`${target.name} recovered health points! `)
+                 
+             } 
+          this.potions--
+        //  console.log("No more potions!")
+
     } 
-    block(){
-        let reduce = damage - this.block
-        
-    }
+    bluePotion(target){
+        if(this.manapotions > 0 && target.mana < target.maxMana ){
+            target.mana += 50
+         if(target.mana < target.maxMana){
+             target.mana = target.maxMana
+         }   
+
+        } 
+        this.manapotions-- 
+        console.log(`${target} recovered mana points!`)
+    }  
+
+    magicAttack(target){
+        if(this.mana>= 70){
+
+            let magicalDamage = Math.floor(Math.random()*(this.maxMag-this.minMag + 1)+this.minMag);
+            target.health -= magicalDamage
+            console.log(`${target.name} received amount of ${magicalDamage} damage!`)
+            this.mana -= 70
+
+        } else{
+            console.log(`${this.name} doesn't have enough mana points!`)
+        }
+    } 
+   
 }
 
 // Heroes and Villains
 
-const sephiroth = new Heroes('Sephiroth',2000,2000,400,400,10,50,30,70,20);
+const sephiroth = new Heroes('Sephiroth',2000,2000,400,400,10,50,30,70,5,5);
 
-const cloud = new Heroes('Cloud', 200, 200, 100, 100, 20,50,30,80,40);
+const cloud = new Heroes('Cloud', 200, 200, 100, 100, 20,50,30,80,5,5);
 
-const tifa = new Heroes('Tifa', 150, 150,200,200,12,35,60,120,30);
+const tifa = new Heroes('Tifa', 150, 150,200,200,12,35,60,120,5,5);
 
 // END OF CLASS OBJECT
 
@@ -62,6 +80,7 @@ console.log($heal)
 let currentTurn = 0;
 
 
+
 // SoundEffects
 
 const hit = new Audio('./music/soundfx/hit.wav')
@@ -70,7 +89,7 @@ const hit = new Audio('./music/soundfx/hit.wav')
 
 
 
-//Cloud Attacks
+//Cloud Attacks && Status
 const cloudAttack =() =>{
     cloud.attack(sephiroth);   
     $cloud.attr('src', './images/Cloud/cloud-attack.gif')
